50d5e83cc51630406ee6db33c42ef536f3188058
Tomas Puza <tomas.puza@inteliments.com>
2022-06-02 17:36:14 +0200

    Update MDB to v2

--- src/app/pages/empty.page.ts
+++ src/app/pages/empty.page.ts
@@ -3,24 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empty-page-component',
   template: `<div>
-  <div>
-    <div mdbDropdown class="dropdown">
-      <button
-        class="btn btn-primary dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        aria-expanded="false"
-        mdbDropdownToggle
-      >
-        Dropdown button
-      </button>
-      <ul mdbDropdownMenu class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
+    <div>
+      <div mdbDropdown class="dropdown">
+        <button
+          class="btn btn-primary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          aria-expanded="false"
+          mdbDropdownToggle
+        >
+          Dropdown button inside router
+        </button>
+        <ul mdbDropdownMenu class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
   </div>`
 })
 export class EmptyPageComponent {
